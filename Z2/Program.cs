c60846530f41439fa176d5617087c10c2f1d7c40
Maksim <you@epic.twod@mail.ru>
2022-07-23 15:33:50 +0300

    Task 2

---
+++ Z2/Program.cs
@@ -0,0 +1,17 @@
+﻿int GetInt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int countdigits(int num)
+{
+    if (num > 9)
+    {
+        return 1+countdigits(num / 10);
+    }
+    else return 1;
+}
+
+int num = GetInt("Enter number: ");
+Console.WriteLine("Answer is: " + countdigits(num));
