ebd9039f8dd656514281de9e0e0afc7de9e5f053
Maksim <you@epic.twod@mail.ru>
2022-07-23 14:49:49 +0300

    Task 1

---
+++ Z1/Program.cs
@@ -0,0 +1,16 @@
+﻿int GetInt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int findsum(int a, int b)
+{
+    if(a!=b) return findsum(a+1, b)+a;
+    else return a;
+}
+
+int a = GetInt("Enter N: ");
+int b = GetInt("Enter M: ");
+
+Console.Write("Answer is: " + findsum(a, b));
